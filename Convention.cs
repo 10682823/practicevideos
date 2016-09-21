2d27e00e12205d2b0a4003c5d9bc2f8b177d19e2
10682823 <burrows.rachel3@gmail.com>
2016-09-20 21:27:07 -0600

    ConventionsScripts

---
+++ Convention.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Convention : MonoBehaviour
+{
+	void Start ()
+	{
+		//this is used to tell me the x position of my object
+		Debug.Log(transform.position.x);
+
+		if(transform.position.y <= 9f)
+		{
+			Debug.Log ("ground");
+		}
+	}
+}
