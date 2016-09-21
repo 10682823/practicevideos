23c507d69552d6e976f622a9653baa2d81207139
10682823 <burrows.rachel3@gmail.com>
2016-09-20 21:27:51 -0600

    SwitchScripts

---
+++ Switch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour 
+{
+	public int statement = 5;
+
+
+	void Greet()
+	{
+		switch (statement)
+		{
+		case 5:
+			print ("How is your day?");
+			break;
+		case 4:
+			print ("What do you have planned for today?");
+			break;
+		case 3:
+			print ("What a beautiful day!");
+			break;
+		case 2:
+			print ("Why did I have to get up today?");
+			break;
+		case 1:
+			print ("I want to leave work.");
+			break;
+		default:
+			print ("Not a greeting.");
+			break;
+		}
+
+	}
+}
