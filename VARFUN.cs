e3ec52dab600b7ae024fb3b0056562e5c167cfd0
10682823 <burrows.rachel3@gmail.com>
2016-09-20 21:28:41 -0600

    VariableAndFunctionsScripts

---
+++ VARFUN.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VARFUN : MonoBehaviour
+{   
+	int myInt = 8;
+
+
+	void Start ()
+	{
+		myInt = MultiplyByTwo(myInt);
+		Debug.Log (myInt);
+	}
+
+
+	int MultiplyByTwo (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+}
