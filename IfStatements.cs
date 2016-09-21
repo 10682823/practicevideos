64a73ec874ecfbd7bea92a3254e08f7b8c3aa6ce
10682823 <burrows.rachel3@gmail.com>
2016-09-20 21:26:33 -0600

    IFStatementsScripts

---
+++ IfStatements.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatements : MonoBehaviour
+{
+	float weatherTemperature = 95.0f;
+	float hotLimitTemperature = 60.0f;
+	float coldLimitTemperature = 30.0f;
+
+
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+
+		weatherTemperature -= Time.deltaTime * 5f;
+	}
+
+
+	void TemperatureTest ()
+	{
+
+		if(weatherTemperature > hotLimitTemperature)
+		{
+			// ... do this.
+			print("The weather is too hot.");
+		}
+
+		else if(weatherTemperature < coldLimitTemperature)
+		{
+			// ... do this.
+			print("The weather is too cold.");
+		}
+
+		else
+		{
+			// ... do this.
+			print("The weather is perfect.");
+		}
+	}
+}
