326c27cd3b0babb45881b8ef3bd9d396c03fadc0
10682823 <burrows.rachel3@gmail.com>
2016-09-23 21:07:33 -0600

    Scope2

---
+++ AnotherClass.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnotherClass
+{
+	public int apples;
+	public int bananas;
+
+
+	private int stapler;
+	private int paper;
+
+
+	public void FruitMachine (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("Fruit total: " + answer);
+	}
+
+
+	private void OfficeSort (int a, int b)
+	{
+		int answer;
+		answer = a + b;
+		Debug.Log("Supplies total: " + answer);
+	}
+}
---
+++ ScopeAccessModifiers.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScopeAccessModifiers : MonoBehaviour
+{
+	public int one = 5;
+
+
+	private int two = 0;
+	private int three = 5;
+
+
+	private AnotherClass myOtherClass;
+
+
+	void Start ()
+	{
+		one = 29;
+
+		myOtherClass = new AnotherClass();
+		myOtherClass.FruitMachine(one, myOtherClass.apples);
+	}
+
+
+	void Example (int pens, int crayons)
+	{
+		int answer;
+		answer = pens * crayons * one;
+		Debug.Log(answer);
+	}
+
+
+	void Update ()
+	{
+		Debug.Log("One is set to: " + one);
+	}
+}
